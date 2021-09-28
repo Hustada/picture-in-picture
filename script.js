ca8a78b3743e59cdeca99b279da98643e2fabaec
hustada <mark.hustad@protonmail.com>
2021-09-28 07:12:32 -0500

    more

--- script.js
+++ script.js
@@ -15,5 +15,15 @@ async function selectMediaStream() {
 }
 
 
+button.addEventListener('click', async () => {
+  // Disable button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset button
+  button.disabled = false;
+  // selectMediaStream();
+})
+
 //On Load
 selectMediaStream();
